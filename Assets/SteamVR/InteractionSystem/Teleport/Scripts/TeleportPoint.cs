32451f41a285593dec65234fa3fc4bd6343d2cfa
olijuseju <jjpeajar@epsg.upv.es>
2022-12-16 16:56:51 +0100

    piedra

--- Assets/SteamVR/InteractionSystem/Teleport/Scripts/TeleportPoint.cs
+++ Assets/SteamVR/InteractionSystem/Teleport/Scripts/TeleportPoint.cs
@@ -225,6 +225,7 @@ namespace Valve.VR.InteractionSystem
 		{
 			if ( !string.IsNullOrEmpty( switchToScene ) )
 			{
+				UnityEngine.SceneManagement.SceneManager.LoadScene(switchToScene);
 				Debug.Log("<b>[SteamVR Interaction]</b> TeleportPoint: Hook up your level loading logic to switch to new scene: " + switchToScene, this);
 			}
 			else
